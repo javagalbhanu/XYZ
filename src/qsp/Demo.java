bb2bd446d8f5d95c3dd5c6189000f4e7f931a4cf
javagalbhanu <javagalbhanu@gmail.com>
2017-03-09 10:58:17 +0530

    Uplaoding AFW after designing by lead...

---
+++ src/qsp/Demo.java
@@ -0,0 +1,12 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void testA(){
+		Reporter.log("executing testA",true);
+	}
+}
